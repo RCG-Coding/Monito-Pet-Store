fd5fcf6d5a5ea02c7ee23ab4367be392a1d31a9d
Roshan Chathuranga <147641397+Roshan-Coding@users.noreply.github.com>
2024-10-21 21:38:45 +0530

    set category page for phone size and laptop size

--- src/component/Category.js
+++ src/component/Category.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; 
-import 'swiper/css/navigation'; 
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
 
 import Header from '../Header';
 import Flooter from '../Flooter';
@@ -12,7 +12,6 @@ import image7 from '../images/image7.png';
 const Category = () => {
   const [pets, setPets] = useState([]);
   const [bgImage, setBgImage] = useState(image7);
-  const [showSwiper1, setShowSwiper1] = useState(false);
 
   const fetchPets = async () => {
     try {
@@ -37,10 +36,10 @@ const Category = () => {
 
     const updateImage = () => {
       const width = window.innerWidth;
-      if (width >= 768) {
-        setBgImage(image4);
-      } else {
+      if (width <= 768) {
         setBgImage(image7);
+      } else {
+        setBgImage(image4);
       }
     };
 
@@ -51,18 +50,18 @@ const Category = () => {
 
   }, [])
 
-  const loadMorePets = () => {
-    setShowSwiper1(true); 
-  };
+  // const loadMorePets = () => {
+  //   setShowSwiper1(true); 
+  // };
 
   return (
     <div>
-      <div className='px-24'>
+      <div className='px-4 md:px-24'>
         <Header />
       </div>
 
       {/* <div className='md:px-24 mt-10' */}
-      <div className='md:px-24 mt-10'>
+      <div className='px-4 md:px-24 mt-10'>
         {/* create baner and giding path */}
         <div className=''>
           <div className='flex items-center space-x-3'>
@@ -99,20 +98,20 @@ const Category = () => {
             <p>Small Dog</p>
           </div>
           {/* create baner */}
-          <div className='md:h-[360px] pt-5 rounded-3xl mt-2' style={{ backgroundImage: `url(${bgImage})` }}>
-            <div className='mt-10 md:text-right text-center px-10 text-white'>
-              <h1 className=" text-5xl font-bold mr-8">One More Friend</h1>
-              <h3 className=" text-4xl font-bold mt-3 mr-8">Thousands More Fun!</h3>
-              <p className=" my-5 text-[12px] md:mr-8">
+          <div className='h-[440px] md:h-[360px] pt-5 rounded-3xl mt-2 w-full' style={{ backgroundImage: `url(${bgImage})`, backgroundSize:'cover' }}>
+            <div className='mt-5 md:mt-10 md:text-right px-6 md:px-9 md:text-white text-blue-deep'>
+              <h1 className="text-4xl md:text-5xl font-bold md:mr-8">One More Friend</h1>
+              <h3 className="text-3xl md:text-4xl font-bold mt-3 md:mr-8">Thousands More Fun!</h3>
+              <div className="md:ml-[670px] my-5 text-[12px] md:mr-8">
                 Having a pet means you have more joy, a new friend, a happy
-                person<br /> who will always be with you to have fun. We have 200+
-                different pets<br /> that can meet your needs!.
-              </p>
-              <div className="flex ml-8 space-x-5 md:ml-[46rem]">
-                <button className="border-white border-2 text-white flex py-2 px-8 md:px-5 rounded-full">
+                person who will always be with you to have fun. We have 200+
+                different pets that can meet your needs!.
+              </div>
+              <div className="flex ml-[2px] space-x-5 md:ml-[46rem]">
+                <button className="md:border-white border-blue-950 text-blue-950 font-semibold border-2 md:text-white flex py-2 px-8 md:px-5 rounded-full">
                   View Intro
                   <svg
-                    className="ml-2 h-6 w-6 text-white"
+                    className="ml-2 h-6 w-6 md:text-white text-blue-950"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -132,7 +131,7 @@ const Category = () => {
                   </svg>
                 </button>
 
-                <button className="ml-3 py-2 px-8 md:px-8 rounded-full bg-white text-gray-800 font-semibold">
+                <button className="ml-3 py-2 px-8 md:px-8 rounded-full md:bg-white bg-blue-950 text-gray-100 md:text-gray-800 md:font-semibold">
                   Explore Now
                 </button>
               </div>
@@ -141,9 +140,9 @@ const Category = () => {
         </div>
 
         {/* create two divition for pets and filter */}
-        <div className='flex mt-6'>
+        <div className='flex flex-col md:flex-row mt-6'>
           {/* create filter division*/}
-          <div className='w-1/4 pr-3'>
+          <div className='w-1/4 pr-3 hidden md:flex flex-col '>
             <h1 className='text-blue-950 font-bold text-2xl'>Filter</h1>
 
             <div className='py-3 border-b-2'>
@@ -236,65 +235,56 @@ const Category = () => {
           </div>
 
           {/* create pet cards division*/}
-          <div className='w-3/4'>
+          <div className='md:w-3/4'>
             {/* create header in pet cards division*/}
-            <div className='flex justify-between items-center'>
-              <div className='flex items-center space-x-3'>
-              <p className='text-2xl text-blue-950 font-bold'>Small Dog</p>
-              <p className='text-blue-950'>52 pupies</p>
+            <div className='flex flex-col md:flex-row md:justify-between md:items-center'>
+              <div className='order-2 md:order-1 flex items-center space-x-3'>
+                <p className='text-2xl text-blue-950 font-bold'>Small Dog</p>
+                <p className='text-blue-950'>52 pupies</p>
               </div>
-              <button className='flex gap-2 px-2 py-1 border-2 items-center rounded-full'
-              onClick={loadMorePets}>
-                Sort by Popular
-                <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
-                </svg>
 
-              </button>
-            </div>
-
-
-            {/* Static Pets Grid: Responsive layout with 2 column (sm), 4 columns (md/lg) */}
-          {pets.length > 0 ? (
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 mt-6 bg-gray-100 p-2 rounded-lg">
-              {pets.slice(0, 15).map((pet) => (
-                <div key={pet.id} className="bg-white shadow-lg rounded-lg overflow-hidden p-2">
-                  <img src={pet.image} alt='petImage' className="w-full h-48 object-cover" />
-                  <div className="p-4">
-                    <h3 className="text-xl font-bold">{pet.breed}</h3>
-                    <div className='flex space-x-2'>
-                    <p className="text-gray-600 text-sm">Gender: {pet.gender}</p>
-                    <p className="text-gray-600 text-sm">Age: {pet.age}</p>
-                    </div>
-                    <p className="text-gray-800 font-semibold">${pet.price}</p>
-                  </div>
+              <div className='mb-5 mt-2 md:mb-0 md:mt-0 flex justify-between items-center order-1 md:order2'>
+                <button className='flex gap-2 px-2 py-1 border-2 items-center rounded-full'
+                >
+                  Sort by Popular
+                  <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
+                  </svg>
+                </button>
+                <div className='flex md:hidden gap-1'>
+                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z" />
+                  </svg>
+                  <p className='text-blue-950 text-xl font-bold'>Filter</p>
                 </div>
-              ))}
-            </div>
-          ) : (
-            <p>Loading pets...</p>
-          )}
+              </div>
 
-          {/* Swiper for additional pets, only shown after "View More" is clicked */}
-          {showSwiper1 && (
-            <div className="mt-8">
-              <Swiper spaceBetween={16} slidesPerView={1} onSwiper={(swiper) => console.log(swiper)}>
-                {pets.slice(8).map((pet) => (
-                  <SwiperSlide key={pet.id}>
-                    <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-                      <img src={pet.image} alt='petImage' className="w-full h-48 object-cover" />
-                      <div className="p-4">
-                        <h3 className="text-xl font-bold">{pet.breed}</h3>
-                        <p className="text-gray-600">{pet.age}</p>
-                        <p className="text-gray-600">{pet.gender}</p>
-                        <p className="text-gray-800 font-semibold">${pet.price}</p>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
             </div>
-          )}
+
+
+            {/*Static Pets Grid: Responsive layout with 2 column (sm), 4 columns (md/lg) */}
+            {pets.length > 0 ? (
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 mt-6 bg-gray-100 p-2 rounded-lg">
+                {pets.slice(0, 15).map((pet) => (
+                  <div key={pet.id} className="bg-white shadow-lg rounded-lg overflow-hidden p-2">
+                    <img src={pet.image} alt='petImage' className="w-full h-48 object-cover" />
+                    <div className="p-4">
+                      <h3 className="text-xl font-bold">{pet.breed}</h3>
+                      <div className='flex space-x-2'>
+                        <p className="text-gray-600 text-sm">Gender: {pet.gender}</p>
+                        <p className="text-gray-600 text-sm">Age: {pet.age}</p>
+                      </div>
+                      <p className="text-gray-800 font-semibold">${pet.price}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>Loading pets...</p>
+            )}
+
+
+
 
 
 
